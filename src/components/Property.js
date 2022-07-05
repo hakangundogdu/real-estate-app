b0cf6ff5958fc1a6bbf6724d9097414e387426b5
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-07-05 03:17:03 +0300

    featured Listings

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,7 +1,6 @@
 import {
   Box,
   SimpleGrid,
-  Button,
   Text,
   CircularProgress,
   Center,
@@ -11,30 +10,31 @@ import { useSelector } from 'react-redux';
 import PropertyBox from './PropertyBox';
 
 const Property = () => {
-  const listings = useSelector((state) => state.listing);
+  const properties = useSelector((state) => state.listing.properties);
   const isLoading = useSelector((state) => state.listing.isLoading);
+  const isSearched = useSelector((state) => state.listing.isSearched);
 
-  const { properties } = listings;
   return (
     <Box my="8">
-      <Text mt={2} fontSize="2xl" fontWeight="semibold" lineHeight="short">
-        Latest Listings
+      <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+        {!isSearched && !isLoading && 'Featured Listings in London'}
+        {isSearched && !isLoading && 'Properties Found'}
       </Text>
 
-      <Button colorScheme="green" size="sm" variant="link">
+      {/* <Button colorScheme="green" size="sm" variant="link">
         <a href="/">View All Listings</a>
-      </Button>
-      {isLoading ? (
+      </Button> */}
+      {isLoading && (
         <Center w="100%">
           <CircularProgress isIndeterminate color="green.300" />
         </Center>
-      ) : (
-        <SimpleGrid my="4" columns={3} spacing={4}>
-          {properties.map((property) => (
-            <PropertyBox property={property} key={property.listing_id} />
-          ))}
-        </SimpleGrid>
       )}
+
+      <SimpleGrid my="4" columns={3} spacing={4}>
+        {properties.map((property) => (
+          <PropertyBox property={property} key={property.listing_id} />
+        ))}
+      </SimpleGrid>
     </Box>
   );
 };
