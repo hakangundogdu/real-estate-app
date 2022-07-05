482e9b82c8a2948bb9cdbcda027615c2c60ace95
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-07-05 15:33:33 +0300

    Property detail page

--- src/pages/PropertyDetail.js
+++ src/pages/PropertyDetail.js
@@ -1,72 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { listingActions } from '../store/listing-slice';
 import {
   Box,
   Image,
   Flex,
   Badge,
-  IconButton,
   Text,
-  Center,
+  HStack,
+  VStack,
+  Stack,
+  Button,
 } from '@chakra-ui/react';
-import { BiBath, BiBed, BiHeart } from 'react-icons/bi';
+import { BiBath, BiBed, BiHeart, BiPhone } from 'react-icons/bi';
 import FallbackImage from '../assets/fallback.png';
 import millify from 'millify';
 
 const PropertyDetail = () => {
-  const dispatch = useDispatch();
   const [properties, setProperties] = useState(
     JSON.parse(window.localStorage.getItem('Property List'))
   );
 
   const params = useParams();
 
-  // useEffect(() => {
-  //   const data = window.localStorage.getItem('Property List');
-  //   if (data !== null)
-  //     dispatch(
-  //       listingActions.setList({
-  //         properties: JSON.parse(data),
-  //       })
-  //     );
-  // }, [dispatch]);
-
   const property = properties.find(
     (listing) => listing.listing_id === params.listing_id
   );
 
   return (
-    <Center>
-      <Box role="group" p="5" borderRadius="2xl" borderWidth="1px">
-        <Box position="relative">
-          <IconButton
-            display="none"
-            _hover={{ bg: 'green.400', color: 'white' }}
-            bg="gray.100"
-            shadow="lg"
-            _groupHover={{ display: 'flex' }}
-            position="absolute"
-            top="2"
-            right="2"
-            aria-label="Like"
-            icon={<BiHeart />}
-            z-index="10"
-          />
-          <Image
-            borderRadius="xl"
-            objectFit="cover"
-            alt="house"
-            w="100%"
-            h="255px"
-            src={property.image_354_255_url}
-            fallbackSrc={FallbackImage}
-          />
-        </Box>
+    <HStack role="group" justify="flex-start" my="14" gap="10">
+      <Box position="relative">
+        <Image
+          borderRadius="xl"
+          objectFit="cover"
+          alt="house"
+          w="100%"
+          h="430px"
+          src={property.image_645_430_url}
+          fallbackSrc={FallbackImage}
+        />
+      </Box>
+      <VStack h="100%" gap="4" align="flex-start">
         <Flex align="baseline" mt={2}>
-          <Badge colorScheme="blue"> FOR {property.listing_status}</Badge>
-          <Text
+          <Badge colorScheme="blue" fontSize="md">
+            {' '}
+            FOR {property.listing_status}
+          </Badge>
+          {/* <Text
             ml={2}
             textTransform="uppercase"
             fontSize="sm"
@@ -74,33 +53,53 @@ const PropertyDetail = () => {
             color="pink.800"
           >
             {property.county}{' '}
-          </Text>
+          </Text> */}
         </Flex>
-        <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+
+        <Text mt={2} fontSize="2xl" fontWeight="semibold" lineHeight="short">
           {property.title}{' '}
         </Text>
-        <Text mt={2}>£{millify(property.price)}</Text>
-
-        <Text mt={2}>
-          {property.displayable_address.length > 45
-            ? property.displayable_address.substring(0, 45) + '...'
-            : property.displayable_address}
+        <Text fontSize="lg" mt={2}>
+          £{millify(property.price)}
         </Text>
+
+        <Text fontSize="lg" mt={2}>
+          {property.displayable_address}
+        </Text>
+
         <Flex
           alignItems="center"
-          mt={2}
+          mt={6}
           justifyContent="flex-start"
           color="green.400"
         >
-          <BiBed size={20} />{' '}
-          <Text fontWeight="semibold" mr={4} ml={2}>
+          <BiBed size={24} />{' '}
+          <Text fontWeight="semibold" fontSize="lg" mr={6} ml={2}>
             {property.num_bedrooms}
           </Text>{' '}
-          <BiBath size={20} />
-          <Text fontWeight="semibold">{property.num_bathrooms}</Text>{' '}
+          <BiBath size={24} />
+          <Text fontWeight="semibold" fontSize="lg">
+            {property.num_bathrooms}
+          </Text>{' '}
         </Flex>
-      </Box>
-    </Center>
+        <Stack direction="row" spacing={4}>
+          <Button
+            leftIcon={<BiHeart size={20} />}
+            colorScheme="green"
+            variant="solid"
+          >
+            Save
+          </Button>
+          <Button
+            leftIcon={<BiPhone size={20} />}
+            colorScheme="gray"
+            variant="solid"
+          >
+            Call Agent
+          </Button>
+        </Stack>
+      </VStack>
+    </HStack>
   );
 };
 
