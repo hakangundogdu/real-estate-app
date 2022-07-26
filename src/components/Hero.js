3b96668d1b4abfe24b7326eb5153b9c15f0a1023
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-07-26 15:58:05 +0300

    search modal

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
 import {
   Stack,
   Flex,
   Button,
-  Box,
   Text,
   Spacer,
   Input,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverHeader,
-  PopoverBody,
-  PopoverFooter,
-  PopoverArrow,
-  PopoverCloseButton,
-  PopoverAnchor,
   useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from '@chakra-ui/react';
 
 import { fetchListingData, listingActions } from '../store/listing-slice';
@@ -27,6 +24,9 @@ export default function Hero() {
   const [location, setLocation] = useState('');
   const dispatch = useDispatch();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = useRef(null);
+  const finalRef = useRef(null);
 
   const locationChangeHandler = (event) => {
     const loc = event.target.value;
@@ -59,110 +59,115 @@ export default function Hero() {
   };
 
   return (
-    <Flex
-      w={'full'}
-      h={'40vh'}
-      backgroundImage={'url(https://source.unsplash.com/178j8tJrNlc/1920)'}
-      backgroundSize={'cover'}
-      backgroundPosition={'center center'}
-      borderRadius="2xl"
-      justify={'center'}
-      align="center"
-    >
+    <>
       <Flex
-        direction="column"
-        align="center"
-        justifyContent="center"
-        bg="gray.800"
-        width={[
-          '100%', // 0-30em
-          'lg', // 30em-48em
-          'xl', // 48em-62em
-          'xl', // 62em+
-        ]}
-        height={[
-          '100%', // 0-30em
-          '40vh', // 30em-48em
-          '200', // 48em-62em
-          '200', // 62em+
-        ]}
-        p={7}
+        w={'full'}
+        h={'40vh'}
+        backgroundImage={'url(https://source.unsplash.com/178j8tJrNlc/1920)'}
+        backgroundSize={'cover'}
+        backgroundPosition={'center center'}
         borderRadius="2xl"
-        color="white"
-        opacity="1"
+        justify={'center'}
+        align="center"
       >
-        <Text
-          fontSize={['2xl', '2xl', '3xl', '3xl']}
-          color="green.400"
-          fontWeight="bold"
+        <Flex
+          direction="column"
+          align="center"
+          justifyContent="center"
+          bg="gray.800"
+          width={[
+            '100%', // 0-30em
+            'lg', // 30em-48em
+            'xl', // 48em-62em
+            'xl', // 62em+
+          ]}
+          height={[
+            '100%', // 0-30em
+            '40vh', // 30em-48em
+            '200', // 48em-62em
+            '200', // 62em+
+          ]}
+          p={7}
+          borderRadius="2xl"
+          color="white"
+          opacity="1"
         >
-          Find Your Dream Home
-        </Text>
-        <Text fontSize={['md', 'md', 'xl', 'xl']} mb="2" fontWeight="semibold">
-          Search properties for sale and to rent in the UK
-        </Text>
-        <Spacer />
-        <Stack direction={['column', 'row']} spacing={4}>
-          <Popover
-            returnFocusOnClose={false}
-            isOpen={isOpen}
-            onClose={onClose}
-            placement="bottom-start"
-            closeOnBlur={true}
+          <Text
+            fontSize={['2xl', '2xl', '3xl', '3xl']}
+            color="green.400"
+            fontWeight="bold"
           >
-            <PopoverTrigger>
-              <Input
-                bg="white"
-                placeholder=" 'London', 'Leeds' or 'Bristol'"
-                focusBorderColor="green.400"
-                width="300px"
-                size="md"
-                variant="outline"
-                color="gray.800"
-                isRequired={true}
-                value={location}
-                onChange={locationChangeHandler}
-                type="search"
-              />
-            </PopoverTrigger>
-            <PopoverContent>
-              <PopoverArrow />
-              <PopoverBody p="4" color="gray.800">
-                Please search 'London', 'Leeds' or 'Bristol'
-                <Box display="flex" justifyContent="flex-end">
-                  {' '}
-                  <Button colorScheme="green" px="8" onClick={onClose}>
-                    OK
-                  </Button>
-                </Box>
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
+            Find Your Dream Home
+          </Text>
+          <Text
+            fontSize={['md', 'md', 'xl', 'xl']}
+            mb="2"
+            fontWeight="semibold"
+          >
+            Search properties for sale and to rent in the UK
+          </Text>
+          <Spacer />
+          <Stack direction={['column', 'row']} spacing={4}>
+            <Input
+              bg="white"
+              placeholder=" 'London', 'Leeds' or 'Bristol'"
+              focusBorderColor="green.400"
+              width="300px"
+              size="md"
+              variant="outline"
+              color="gray.800"
+              isRequired={true}
+              value={location}
+              onChange={locationChangeHandler}
+              type="search"
+              ref={finalRef}
+            />
 
-          <Button
-            onClick={
-              allowedLocations.includes(location)
-                ? searchSaleHandler
-                : popUpHandler
-            }
-            colorScheme="green"
-            variant="solid"
-          >
-            <a href="/">For Sale</a>
-          </Button>
-          <Button
-            onClick={
-              allowedLocations.includes(location)
-                ? searchRentHandler
-                : popUpHandler
-            }
-            colorScheme="green"
-            variant="solid"
-          >
-            <a href="/">To Rent</a>
-          </Button>
-        </Stack>
+            <Button
+              onClick={
+                allowedLocations.includes(location)
+                  ? searchSaleHandler
+                  : popUpHandler
+              }
+              colorScheme="green"
+              variant="solid"
+            >
+              <a href="/">For Sale</a>
+            </Button>
+            <Button
+              onClick={
+                allowedLocations.includes(location)
+                  ? searchRentHandler
+                  : popUpHandler
+              }
+              colorScheme="green"
+              variant="solid"
+            >
+              <a href="/">To Rent</a>
+            </Button>
+          </Stack>
+        </Flex>
       </Flex>
-    </Flex>
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Testing Mode</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Please search 'London', 'Leeds' or 'Bristol'.</ModalBody>
+          <ModalFooter>
+            <Button colorScheme="green" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
