87e6faa3275a5dcf84c93705c153c847d4a762ed
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-07-26 13:00:58 +0300

    Scroll to top button

---
+++ src/components/Up.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { FaCaretUp } from 'react-icons/fa';
+import { IconButton } from '@chakra-ui/react';
+
+const Up = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+  return (
+    <>
+      {isVisible && (
+        <IconButton
+          bg="gray.100"
+          shadow="lg"
+          position="fixed"
+          bottom="10"
+          right="48"
+          icon={<FaCaretUp />}
+          z-index="10"
+          onClick={scrollToTop}
+        />
+      )}
+    </>
+  );
+};
+
+export default Up;
